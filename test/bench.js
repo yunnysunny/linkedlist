51bb3d3f00ea84d512055fa7fa869f27eb8e41d5
yunnysunny <yunnysunny@gmail.com>
2018-03-01 18:19:46 +0800

    fixed the bench script error

--- test/bench.js
+++ test/bench.js
@@ -2,18 +2,34 @@ const async = require('async');
 const expect = require('chai').expect;
 const LRUList = require('../node').LRUList;
 
-const LIST_SIZE = 1000;
-const TEST_SIZE = 1000000;
-const list = new LRUList(LIST_SIZE);
+const LIST_SIZE = 8192;
+const TEST_SIZE = 102400;
+var list = new LRUList(LIST_SIZE);
 
-describe('native lru benchmark# ',function(done) {
-    it(`test for loop of ${TEST_SIZE}`,function() {
+describe('native lru benchmark# ',function() {
+    it(`test for loop of ${TEST_SIZE}`,function(done) {
         async.times(TEST_SIZE,function(index,callback) {
-            list.addOne(Math.random(),function() {
+            const begin=Date.now();
+            list.addOne(Math.random()+'',function() {
+                const used = Date.now()-begin;
+                if (used>1000) {
+                    //console.warn('used much time',used,index);
+                }
                 callback();
             });
         },function() {
             expect(list.size()).to.be.equal(LIST_SIZE);
+            // list = null;
+            done();
+        });
+    });
+    it('test after bench',function(done) {
+        const begin=Date.now();
+        list.addOne(Math.random()+'',function() {
+            const used = Date.now()-begin;
+            if (used>1000) {
+                //console.warn('used much time',used,index);
+            }
             done();
         });
     });
